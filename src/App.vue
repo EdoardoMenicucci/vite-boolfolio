6c30678e70c302f3b5a4a166951bdb53340bcceb
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-07-18 16:20:21 +0200

    milestone 3 vue

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      projects: [],
+      baseUrl: 'http://localhost:8000'
+    }
+  },
+
+  methods: {
+  },
+
+  mounted() {
+    axios.get(this.baseUrl + '/api/projects').then((response) => {
+      console.log(response.data);
+      this.projects = response.data.projects.data;
+    })
+  },
+
+  computed: {
+  },
+
+}
+
+
+</script>
+
+<template>
+  <div class="container-fluid bg-dark d-flex flex-wrap justify-content-center">
+    <div class="card m-3" v-for="project in projects">
+      <div class="card-header">{{ project.name }}</div>
+
+      <!-- SE L'IMMAGINE INIZIA CON HTTP -->
+      <!--  v-if="startsWhit(project.img, 'http')"-->
+      <img v-if="project.img.startsWith('http')" :src="project.img" alt="">
+      <!-- <img :src="project.img" alt=""> -->
+      <!-- SE L'IMMAGINE E' NELLO STORAGE DI LARAVEL -->
+      <img v-else :src="baseUrl + '/storage/' + project.img" alt="">
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'HelloWorld',
+
+  data() {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  mounted() {
+  },
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped></style>
